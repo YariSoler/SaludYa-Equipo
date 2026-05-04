e15cb381a08fa57711ef480e1b1c82d95b603634
Yaridiveth <yithosoler15@gmail.com>
2026-05-03 19:13:37 -0500

    Actualizo app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+
 import Login from "./pages/Login";
-import RegisterPaciente from "./pages/RegisterPaciente";
-import RegisterMedico from "./pages/RegisterMedico";
-
-import DashboardPaciente from "./pages/DashboardPaciente";
-import DashboardMedico from "./pages/DashboardMedico";
-import DashboardAdmin from "./pages/DashboardAdmin";
-
-import AgendarCita from "./pages/AgendarCita";
 import RecoverPassword from "./pages/RecoverPassword";
 
-import PerfilPaciente from "./pages/PerfilPaciente";
+
+import RegisterPaciente from "./pages/RegisterPaciente";
+
+
+import DashboardAdmin from "./pages/DashboardAdmin";
+import DashboardMedico from "./pages/DashboardMedico";
+import DashboardPaciente from "./pages/DashboardPaciente";
+
 import CrearMedico from "./pages/CrearMedico";
+import AgendarCita from "./pages/AgendarCita";
+import PerfilPaciente from "./pages/PerfilPaciente";
 
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
 
-        <Route path="/register-paciente" element={<RegisterPaciente />} />
-        <Route path="/register-medico" element={<RegisterMedico />} />
-
-        <Route path="/dashboard-paciente" element={<DashboardPaciente />} />
-        <Route path="/dashboard-medico" element={<DashboardMedico />} />
-        <Route path="/dashboard-admin" element={<DashboardAdmin />} />
-
-        <Route path="/agendar-cita" element={<AgendarCita />} />
         <Route path="/recover" element={<RecoverPassword />} />
 
-        <Route path="/perfil" element={<PerfilPaciente />} />
+        <Route path="/register-paciente" element={<RegisterPaciente />} />
+
+        <Route path="/dashboard-admin" element={<DashboardAdmin />} />
         <Route path="/crear-medico" element={<CrearMedico />} />
+
+        <Route path="/dashboard-medico" element={<DashboardMedico />} />
+
+        <Route path="/dashboard-paciente" element={<DashboardPaciente />} />
+        <Route path="/agendar-cita" element={<AgendarCita />} />
+        <Route path="/perfil" element={<PerfilPaciente />} />
+
       </Routes>
     </Router>
   );
